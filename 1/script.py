da6d764325fe4e4f8b99f9759279a151ab5918cd
Jade Chen <jfchen29@berkeley.edu>
2025-09-02 15:26:36 -0700

    dolly

--- 1/script.py
+++ 1/script.py
@@ -1,10 +1,15 @@
 import imageio
 import os
 
-images = [os.path.join("dolly", f) for f in os.listdir("dolly") if f.endswith(".jpg")]
+base_dir = os.path.dirname(os.path.abspath(__file__))
+dolly_dir = os.path.join(base_dir, "dolly")  
+
+images = [os.path.join(dolly_dir, f) for f in os.listdir(dolly_dir) if f.endswith(".jpg")]
 
 media = []
 for filename in images:
     media.append(imageio.imread(filename))
-imageio.mimsave('dollyzoom.gif', media)
+imageio.mimsave('dollyzoom.gif', media, loop=0)
 
+
+# cite: stackoverflow: https://stackoverflow.com/questions/753190/programmatically-generate-video-or-animated-gif-in-python
