dd31970c49be11a16d45a9d948ce996e810f42f7
Jade Chen <jfchen29@berkeley.edu>
2025-09-12 04:37:35 -0700

    comments

--- 1/main.py
+++ 1/main.py
@@ -5,7 +5,9 @@ import skimage.io as skio
 import os
 
 jpg = ["cathedral.jpg", "monastery.jpg", "tobolsk.jpg"]
+# emir uses edge for better performance
 tif_part1 = ["church.tif", "emir.tif", "harvesters.tif", "icon.tif", "italil.tif"] 
+# lastochikino and lugano uses edge for better performance
 tif_part2 = ["lastochikino.tif", "lugano.tif", "melons.tif", "self_portrait.tif", "siren.tif", "three_generations.tif"]
 extra = ["detali_sobora.tif", "locomotive.tif", "monrepos_park.tif", "village_dagestan.tif"]
 fix = ["lastochikino.tif", "lugano.tif", "emir.tif"]
@@ -13,7 +15,7 @@ fix = ["lastochikino.tif", "lugano.tif", "emir.tif"]
 def picture_dimensions(pic_list):
     for pic in pic_list:
         write(pic)
-        imname = '1/extra/'+pic
+        imname = '1/proj1_data/'+pic
         im = skio.imread(imname)
         im = sk.img_as_float(im)
         height = np.floor(im.shape[0] / 3.0).astype(int)
@@ -27,13 +29,17 @@ def picture_dimensions(pic_list):
         g_cropped = g[int(col*0.1): int(col - 0.1*col), int(0.1*row):int(row - 0.1*row)] 
         r_cropped = r[int(col*0.1): int(col - 0.1*col), int(0.1*row):int(row - 0.1*row)] 
         
-        #ag = align_ncc(b_cropped, g_cropped, 0, 0)
-        ag = pyramid_align(b_cropped, g_cropped, 5, False)
+        ag = align_ncc(b_cropped, g_cropped, 0, 0) # use for naive align (low-res)
+
+        # ag = pyramid_align(b_cropped, g_cropped, 5, False) # uncomment for tif 
+        # specific images that don't work with regular pyramid_ncc_align need to use edge mapping instead -> change False to True
         write("green shift: " + str(ag))
         g_shifted = np.roll(np.roll(g, ag[1], axis=0), ag[0], axis=1)
 
-        #ar = align_ncc(b_cropped, r_cropped, 0, 0)
-        ar = pyramid_align(b_cropped, r_cropped, 5, False)
+        ar = align_ncc(b_cropped, r_cropped, 0, 0) # use for naive align (low-res)
+
+        # ar = pyramid_align(b_cropped, r_cropped, 5, False) # uncomment for tif
+        # specific images that don't work with regular pyramid_ncc_align need to use edge mapping instead -> change False to True
         write("red shift: " + str(ar))
         r_shifted = np.roll(np.roll(r, ar[1], axis=0), ar[0], axis=1)
 
@@ -58,4 +64,5 @@ def write(info):
     with open("1/results.txt", "a") as f: 
         f.write(info+'\n')
 
-picture_dimensions(["village_dagestan.tif", "locomotive.tif"])
+picture_dimensions(jpg)
+
